4a85ab57093764e400754b2b6bf0c6b6f9d72a69
Olivier Cavadenti <olivier.cavadenti@gmail.com>
2022-04-23 18:36:06 +0200

    update doc

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # manson
 A Json Library in Kotlin to perform high-level transformations.
+
+JsonPath object transformation :
+
+```kotlin
+val json = Json( """
+        {
+        "b" : 12,
+        "a" : { 
+            "b" : "c", 
+            "aa" : [ {"b" : [1,2,3] } ]
+        }, 
+        "d" : ["dd"]
+    }
+""")
+val jsonObject = json.transform(
+    TransformerFactory.jsonObject(
+        "numeric" to "$.a.aa[0].b[2]",
+        "object" to "$.a",
+        "array" to "$.a.aa[0].b",
+        "char" to "$.a.b",
+        "arrayChar" to "$.d"
+    )
+);
+
+```
+jsonObject.getJsons() contains :
+```json
+{
+    "numeric":3,
+    "b":"c",
+    "aa": [ { "b": [1,2,3] }],
+    "array": [1,2,3],
+    "char":"c",
+    "arrayChar": ["dd"]
+}
+```
--- src/test/kotlin/extraction/JsonObjectTest.kt
+++ src/test/kotlin/extraction/JsonObjectTest.kt
@@ -8,7 +8,16 @@ import kotlin.test.assertEquals
 class JsonObjectTest {
     @Test
     fun shouldConstructJsonObject() {
-        val json = Json("{\"b\" : 12, \"a\" : { \"b\" : \"c\", \"aa\" : [ {\"b\" : [1,2,3] } ]}, \"d\" : [\"dd\"]}")
+        val json = Json( """
+                {
+                "b" : 12,
+                "a" : { 
+                    "b" : "c", 
+                    "aa" : [ {"b" : [1,2,3] } ]
+                }, 
+                "d" : ["dd"]
+            }
+        """)
         val jsonObject = json.transform(
             TransformerFactory.jsonObject(
                 "numeric" to "$.a.aa[0].b[2]",
@@ -19,7 +28,16 @@ class JsonObjectTest {
             )
         )
         assertEquals(
-            listOf("{\"numeric\":3,\"b\":\"c\",\"aa\":[{\"b\":[1,2,3]}],\"array\":[1,2,3],\"char\":\"c\",\"arrayChar\":[\"dd\"]}"),
+            listOf("""
+                {
+                    "numeric":3, 
+                    "b":"c",
+                    "aa": [ { "b": [1,2,3] }],
+                    "array": [1,2,3],
+                    "char":"c",
+                    "arrayChar": ["dd"]
+                }"""
+            ),
             jsonObject.getJsons()
         )
     }
